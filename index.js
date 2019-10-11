bd7aa2558610a646611ea1f28f56605e9e7ce3e6
Jake Rose <jake1020@mac.com>
2019-10-11 18:58:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -19,10 +19,12 @@ function sayHiToGrandma(string) {
     return "I can't hear you!"
 }
 
-if (string.toLowerCase() === string){
+function sayHiToGrandma(string) {
+  if (string.toLowerCase() === string)
     return "YES INDEED!"
 }
 
-if (string === "I love you, Grandma.") {
+function shout(string) {
+  if (string === "I love you, Grandma.")
     return "I love you, too."
 }
