ce5db65e77e8d04ce03373a9d537e6dece1e3f6d
Jake Rose <jake1020@mac.com>
2019-10-11 19:36:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,16 +14,14 @@ function logWhisper(string) {
   console.log(string.toLowerCase())
 }
 
-// function sayHiToGrandma(string) {
-//   if (string.toLowerCase() === string)
-//     return "I can't hear you!"
-// }
-//
-// function sayHiToGrandma(string) {
-//   if (string.toUpperCase() === string)
-//     return "YES INDEED!"
-// }
-//
+function sayHiToGrandma(string) {
+  if (string.toLowerCase() === string) {
+    return "I can't hear you!"
+}
+  if (string.toUpperCase() === string) {
+    return "YES INDEED!"
+}
+
 // function shout(string) {
 //   if (string === "I love you, Grandma.")
 //     return "I love you, too."
