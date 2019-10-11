04a6e8915a69f40e30d9b535120545c3e855e26f
Jake Rose <jake1020@mac.com>
2019-10-11 19:38:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -19,7 +19,8 @@ function sayHiToGrandma(string) {
     return "I can't hear you!"
 }
   if (string.toUpperCase() === string) {
-    return "YES INDEED!"
+    return "YES INDEED!" 
+}
 }
 
 // function shout(string) {
